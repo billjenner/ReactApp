101b6035dfd409fae9529c1b087b056f70e33603
Bill Jenner <bill.jenner@gmail.com>
2018-11-09 12:21:35 -0800

    Add google OAuth functionality

---
+++ config/keys.js
@@ -0,0 +1,10 @@
+//
+// (public token)
+//client ID         948208241901-ahtoiredpq75236mqocikv2q5i6qdgae.apps.googleusercontent.com
+// (private token)
+// client secret    sAONYnXbHejE7piXxfDo3QeD
+module.exports = {
+    googleClientID: '948208241901-ahtoiredpq75236mqocikv2q5i6qdgae.apps.googleusercontent.com',
+    googleClientSecret: 'sAONYnXbHejE7piXxfDo3QeD'
+};
+
