4dcf0e61ae2f693c4e1fad80dceb7b7a925a32b3
Bill Jenner <bill.jenner@gmail.com>
2018-11-09 12:22:24 -0800

    Add Google OAuth functionality

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+require('./services/passport');
+// old code
+// const authRoutes = require('./routes/authRoutes');
+// authRoutes(app);
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there'});
-});
+require('./routes/authRoutes')(app);
 
+// mkae passport aware of the new strategy
+// need to pass client ID and client Secret
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
